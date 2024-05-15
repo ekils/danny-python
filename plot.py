998da5ff8b6b2b5f8a2853541e12db61033b9a38
ekils <bobobo746@hotmail.com>
2024-05-15 13:54:51 +0000

    2nd

--- plot.py
+++ plot.py
@@ -114,5 +114,6 @@ def postInput():
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+#if __name__ == '__main__':
+   # app.run(host='0.0.0.0', port=3000, debug=True)
+ #  app.run()
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from plot import app
+
+if __name__=="main__":
+    app.run()
